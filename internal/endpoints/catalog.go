98907da6650e07711c21e86151d6c0e2c00d362a
Justin Kolberg <amd.prophet@gmail.com>
2022-02-18 14:04:07 -0800

    remove prompts & resource_patches fields from catalog endpoint
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- internal/endpoints/catalog.go
+++ internal/endpoints/catalog.go
@@ -6,6 +6,20 @@ import (
 
 // GET /api/:generated_sha/v1/catalog.json
 func GenerateCatalogEndpoint(basePath string, nis map[string][]catalogapiv1.IntegrationVersion) error {
+	// TODO(jk): Implement a less hacky way of determing what fields are
+	// rendered for an endpoint. Perhaps using gotemplates for views.
+	//
+	// Set prompts & resource_patches fields to empty strings to prevent them
+	// from being shown in the catalog endpoint.
+	for namespace, versions := range nis {
+		for i, version := range versions {
+			version.Prompts = nil
+			version.ResourcePatches = nil
+
+			nis[namespace][i] = version
+		}
+	}
+
 	catalog := catalogapiv1.Catalog{
 		NamespacedIntegrations: nis,
 	}
