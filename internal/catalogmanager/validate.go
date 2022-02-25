c4c7667bfa0e8d1bbe34ba96ca2f2e35767e2a36
Justin Kolberg <amd.prophet@gmail.com>
2022-02-25 01:05:06 -0800

    validate all integration resources
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- internal/catalogmanager/validate.go
+++ internal/catalogmanager/validate.go
@@ -30,42 +30,45 @@ func (m CatalogManager) ValidateCatalog() error {
 			if err != nil {
 				logger.Err(err).Msg("Failed to load integration config")
 				validationFailed = true
-				continue
 			}
 			if err := integrationConfig.Validate(); err != nil {
 				logger.Err(err).Msg("Failed to validate integration config")
 				validationFailed = true
-				continue
 			}
 
 			// load & validate sensu resources
 			// TODO(jk): call resouces.Validate() once it's implemented
 			if _, err = integrationLoader.LoadResources(); err != nil {
 				logger.Err(err).Msg("Failed to load resources file")
+				validationFailed = true
 			}
 
 			// load & validate logo
 			_, err = integrationLoader.LoadLogo()
 			if err != nil {
 				logger.Err(err).Msg("Failed to load logo")
+				validationFailed = true
 			}
 
 			// load & validate readme
 			_, err = integrationLoader.LoadReadme()
 			if err != nil {
 				logger.Err(err).Msg("Failed to load readme")
+				validationFailed = true
 			}
 
 			// load & validate changelog
 			_, err = integrationLoader.LoadReadme()
 			if err != nil {
 				logger.Err(err).Msg("Failed to load changelog")
+				validationFailed = true
 			}
 
 			// load & validate images
 			_, err = integrationLoader.LoadImages()
 			if err != nil {
 				logger.Err(err).Msg("Failed to load images")
+				validationFailed = true
 			}
 		}
 	}
